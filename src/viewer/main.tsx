fad9dc93cc946c22cd81fc455deb2621aaaeb85c
shakilhossain-cse <shakilhossainp.cse@gmail.com>
2026-05-05 08:26:35 +0600

    fix: remove unused viewer.css import from main.tsx

--- src/viewer/main.tsx
+++ src/viewer/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Viewer } from './Viewer';
-import './viewer.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
